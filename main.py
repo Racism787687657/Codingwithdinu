bb3bfae8dc8913b4aa7e4883d4fd34969b93dc45
Racism787687657 <max.rowlands44@gmail.com>
2023-03-24 13:58:33 +0000

    lesson 1
    Prints, space=space, data types

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-def print_even_numbers():
-    for i in range(0, 10, 2):
-        print(i)
-
+print("hello world")
+# procedural programming
+# variables is an item identified by unique name whose value can be changed
+x = 0
+# a data type is a type of data
+# string,boolean,integer,float
+name = "max"
+x = 17
+male = True
+print(male, x, name)
+male = str(male)
+x = float(17)
+print(male)
+print(x)
+y = "5"
+y = int(y)
+print(y)
